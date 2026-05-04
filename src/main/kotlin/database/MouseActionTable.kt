b8b3fbec02b1fa525bc98e838242e5f752ce88ab
N1gh1Guard <m@dema.ru>
2026-05-04 10:28:21 +0300

    Changing mouse output

--- src/main/kotlin/database/MouseActionTable.kt
+++ src/main/kotlin/database/MouseActionTable.kt
@@ -4,7 +4,7 @@ import org.jetbrains.exposed.v1.core.Table
 
 object MouseActionTable : Table ("MouseActions"){
     val id = long("id").references(ActionTable.id)
-    val deltaX = integer("delta_x").nullable()
-    val deltaY = integer("delta_y").nullable()
-    val button = integer("button").nullable()
+    val deltaX = float("delta_x").nullable()
+    val deltaY = float("delta_y").nullable()
+    val isClick = bool("is_click")
 }
